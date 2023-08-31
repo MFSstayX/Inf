83dc45c3fc7796b08c16b27bc0d160f125eebabc
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-08-31 09:10:42 +0000

    Konto, Girokonto, Sparkonto

--- Konto.java
+++ Konto.java
@@ -1,1 +1,21 @@
-a       a   
+public class Konto {
+    private int kontonr; 
+    private int saldo; 
+    public Konto(int kontonr, int saldo) {
+        this.kontonr = kontonr; 
+        this.saldo = saldo;
+    }
+    public int getKontonr() {
+        return kontonr; 
+    }
+    public void setKontonr(int kontonr) {
+        this.kontonr = kontonr;
+    }
+    public int getSaldo() {
+        return saldo; 
+    }
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+}
