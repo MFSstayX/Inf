f68c97b2d976330e7bf2e13d876a7f6f343b36c4
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-05 06:14:29 +0000

    Main

--- Chipkarte.java
+++ Chipkarte.java
@@ -1,6 +1,13 @@
 public class Chipkarte {
-    private int pin; 
+    private int pin;
+
     public Chipkarte(int pin) {
-        this.pin = pin; 
+        this.pin = pin;
     }
+	public int getPin() {
+		return this.pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main{public static void main(String[] args) {
-    System.out.println("Simon");
-
+    Bank volksbank = new Bank();
+    Kunde kunde = new Kunde("Wetzel", "Paul", "Parselvalstraße 48", 282475, 210907, volksbank);
+    Konto konto = new Konto(23497293, 500, kunde);
+    System.out.println(konto.getSaldo());
+    
 }
 }
