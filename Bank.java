f77c582206cca52d3b20ada5ca256fd022adffb6
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-06 07:23:59 +0000

    neuer Kunde

--- Bank.java
+++ Bank.java
@@ -1,5 +1,15 @@
 public class Bank {
-    public Bank() {
+    private Kunde[] kunden;
+    private int annzahlKunden; 
+    public Bank(Kunde[] kunden, int anzahlKunden) {
+        this.annzahlKunden = anzahlKunden; 
+        kunden[] = new Kunde[1000];
+    }
+    public void neuerKunde(Kunde kunde) {
+        kunden[annzahlKunden] = kunde; 
+        this.annzahlKunden += 1;
+    }
+    public void neuesKonto(String art) {
         
     }
     // Hier müssen einzahlen, auzahlen und überweisen als Methode hin + neue kunden und neue Konten
