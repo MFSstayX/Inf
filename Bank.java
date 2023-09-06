8591d8375c36e3b391ffdac89d21a553f5a8dbde
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-06 07:29:27 +0000

    neuesKonto

--- Bank.java
+++ Bank.java
@@ -1,11 +1,17 @@
 public class Bank {
     private Kunde[] kunden;
     private int annzahlKunden; 
+    private Konto[] kontos;
     public Bank() {
         this.annzahlKunden = 0; 
         kunden = new Kunde[1000];
     }
     public void neuerKunde(String name, String vorname, String anschrift, int geburtsdatum) {
+        int kundennummer = annzahlKunden += 1000; 
+        kunden[annzahlKunden] = new Kunde(name, vorname, anschrift, kundennummer, geburtsdatum);
+        this.annzahlKunden += 1;
+    }
+     public void neuesKonto(String name, String vorname, String anschrift, int geburtsdatum) {
         int kundennummer = annzahlKunden += 1000; 
         kunden[annzahlKunden] = new Kunde(name, vorname, anschrift, kundennummer, geburtsdatum);
         this.annzahlKunden += 1;
