2eee7123134ec5db87491653a847d00c79b25487
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-08 09:03:05 +0000

    k

--- Bank.java
+++ Bank.java
@@ -1,23 +1,47 @@
 public class Bank {
     private Kunde[] kunden;
-    private int anzahlKunden; 
-    private Konto[] kontos;
+    private int anzahlKunden;
+    private Konto[] konten;
     private int anzahlKonten;
+
     public Bank() {
         this.anzahlKunden = 0; 
         this.anzahlKonten = 0;
         kunden = new Kunde[1000];
-        kontos = new Konto[1000];
+        konten = new Konto[2000];
+    }
+     public Kunde[] getKunden() {
+        return this.kunden;
+    }
+    public void setKunden(Kunde[] kunden) {
+        this.kunden = kunden;
+    }
+    public int getAnzahlKunden() {
+        return this.anzahlKunden;
+    }
+    public void setAnzahlKunden(int anzahlKunden) {
+        this.anzahlKunden = anzahlKunden;
+    }
+    public Konto[] getKontos() {
+        return this.konten;
+    }
+    public void setKontos(Konto[] kontos) {
+        this.konten = kontos;
+    }
+    public int getAnzahlKonten() {
+        return this.anzahlKonten;
+    }
+    public void setAnzahlKonten(int anzahlKonten) {
+        this.anzahlKonten = anzahlKonten;
     }
     public void neuerKunde(String name, String vorname, String anschrift, int geburtsdatum) {
         int kundennummer = anzahlKunden += 1000; 
         kunden[anzahlKunden] = new Kunde(name, vorname, anschrift, kundennummer, geburtsdatum);
         this.anzahlKunden += 1;
     }
-     public void neuesKonto(int saldo, Kunde inhaber) {
+     public void neuesKonto(char art, int saldo, Kunde inhaber) {
         int kontonr = anzahlKonten += 1000; 
-        kontos[anzahlKonten] = new Konto(kontonr,  saldo, null);
+        konten[anzahlKonten] = new Konto(kontonr,  saldo, null);
         this.anzahlKonten += 1;
     }
-    // Hier müssen einzahlen, auzahlen und überweisen als Methode hin + neue kunden und neue Konten
 }
