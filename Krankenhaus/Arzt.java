5300d21750333f21b3a0ec3afd902123c866b747
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-19 09:12:51 +0000

    o

--- Krankenhaus/Arzt.java
+++ Krankenhaus/Arzt.java
@@ -3,12 +3,29 @@ package Krankenhaus;
 public class Arzt extends Personal{
     private String fachgebiet; 
     private boolean behandeltPrivatpatienten; 
+
+    public void setBehandeltPrivatpatienten(boolean behandeltPrivatpatienten) {
+        this.behandeltPrivatpatienten = behandeltPrivatpatienten;
+    }
     public Arzt(String pName, String pAnschrift, double pGehalt, String pFachgebiet, boolean pBehandeltPrivatpatienten) {
         super(pName, pAnschrift, pGehalt);
         fachgebiet = pFachgebiet; 
         behandeltPrivatpatienten = pBehandeltPrivatpatienten; 
     }
-    public void setBehandeltPrivatpatienten(boolean x) {
-        behandeltPrivatpatienten = x; 
+    public String getFachgebiet() {
+        return this.fachgebiet;
     }
+
+    public void setFachgebiet(String fachgebiet) {
+        this.fachgebiet = fachgebiet;
+    }
+
+    public boolean isBehandeltPrivatpatienten() {
+        return this.behandeltPrivatpatienten;
+    }
+
+    public boolean getBehandeltPrivatpatienten() {
+        return this.behandeltPrivatpatienten;
+    }
+
 }
--- Krankenhaus/Personal.java
+++ Krankenhaus/Personal.java
@@ -4,9 +4,33 @@ public class Personal {
     private String name; 
     private String anschrift; 
     private double gehalt; 
+
     public Personal(String pName, String pAnschrift, double pGehalt) {
         name = pName; 
         anschrift = pAnschrift; 
         gehalt = pGehalt; 
+    }
+     public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAnschrift() {
+        return this.anschrift;
+    }
+
+    public void setAnschrift(String anschrift) {
+        this.anschrift = anschrift;
+    }
+
+    public double getGehalt() {
+        return this.gehalt;
+    }
+
+    public void setGehalt(double gehalt) {
+        this.gehalt = gehalt;
     }
 }
