c0e262f7deedd273850dd41c0e019acb89a23f8e
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-09-19 09:24:49 +0000

    u

--- Krankenhaus/Main.java
+++ Krankenhaus/Main.java
@@ -2,11 +2,12 @@ package Krankenhaus;
 
 public class Main {
     public static void main(String[] args) {
-        Arzt paul = new Arzt("Pail", "sfgj", 2, "dufggfg", false); 
+        Arzt paul = new Arzt("Pail", "sfgj", 2, "dufggfg", true); 
         Chefarzt p = new Chefarzt("skjfng", "sfjnv", 3, "sifbhuidfsbv", false);
         Krankenhaus k = new Krankenhaus(2.0, 2.0);
         Patient l = new Patient(2.0, false, paul);
         k.behandeln(l, p, 20);
         System.out.println(l.getBisherigeRechnungssumme()); 
+        System.out.println(paul.getBehandeltPrivatpatienten());
     }
 }
