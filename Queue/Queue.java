ef71b3a6e53c3daa873bb2c2b60e6b25ee7b635b
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-10-20 09:49:12 +0000

    a

---
+++ Queue/Queue.java
@@ -0,0 +1,19 @@
+public class Queue{
+    private Node head; 
+    private Node tail; 
+    public Queue() {
+
+    }
+    public void enqueue(Statist statist) {
+
+    }
+    public void dequeue() {
+
+    }
+    public Statist front() {
+        return null; 
+    }
+    public boolean isEmpty() {
+        return null; 
+    }
+}
