f580004d57ebe4407ce020ea6caea520cd14187d
MFSstayX <133868157+MFSstayX@users.noreply.github.com>
2023-10-20 09:45:25 +0000

    f

--- Queue/Node.java
+++ Queue/Node.java
@@ -1,4 +1,26 @@
 public class Node {
-    private Statist content; 
+    private Statist content;
+    private Node next;
+   
+    public Node(Statist content) {
+        this.content = content; 
+    }
     
+     public Statist getContent() {
+        return this.content;
+    }
+
+    public void setContent(Statist content) {
+        this.content = content;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+
 }
